8427980acea97c0fb07a560cbf22eac61b2a6582
Hassan <Hassan1620121@gmail.com>
2021-01-19 21:40:46 +0500

    Created javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("This is index.js");
+//constructor
+function Book(name,author,type){
+    this.name =  name;
+    this.author = author;
+    this.type = type;
+}
+
+
+//Display Constructor
+function Display(){
+
+}
+
+
+//Add methods to display prototype
+
+
+
+
+//Add Submit event Listener to form
